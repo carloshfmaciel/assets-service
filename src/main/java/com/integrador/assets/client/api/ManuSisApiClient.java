386d1c4788ccd085c89a083fd345cbae8545e51c
CARLOS MACIEL EXT <carlos_maciel_ext@carrefour.com>
2023-12-11 15:44:21 -0300

    add description to custom yml properties

--- src/main/java/com/integrador/assets/client/api/ManuSisApiClient.java
+++ src/main/java/com/integrador/assets/client/api/ManuSisApiClient.java
@@ -4,30 +4,33 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ManuSisApiClient {
 
-    public JSONObject fetchAssets() throws Exception {
-        String apiUrl = "https://millshomolog.manusis4.com/api/v1/assets";
-        HttpClient httpClient = HttpClient.newHttpClient();
+	@Value("${api.external.manusis.url}")
+	private String manuSisApiUrl;
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Token token=0c526f8c307a4cbd841da53de59698f1")
-                .header("app-origin", "API")
-                .header("x-language", "pt_BR")
-                .build();
+	@Value("${api.external.manusis.token}")
+	private String manuSisApiToken;
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+	public JSONObject fetchAssets() throws Exception {
+		HttpClient httpClient = HttpClient.newHttpClient();
 
-        if (response.statusCode() >= 200 && response.statusCode() < 300) {
-            return new JSONObject(response.body());
-        } else {
-            throw new RuntimeException("Erro na requisição. Código de resposta: " + response.statusCode());
-        }
-    }
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(manuSisApiUrl))
+				.header("Content-Type", "application/json").header("Authorization", "Token token=" + manuSisApiToken)
+				.header("app-origin", "API").header("x-language", "pt_BR").build();
+
+		HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+		if (response.statusCode() >= 200 && response.statusCode() < 300) {
+			return new JSONObject(response.body());
+		} else {
+			throw new RuntimeException("Erro na requisição. Código de resposta: " + response.statusCode());
+		}
+	}
 }
